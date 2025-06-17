0929fcf8ae2419a83f8f6d6256b2e30b8eaf62a9
Jordony00 <Jordony.Usen@edu.sait.ca>
2025-06-17 13:59:18 -0600

    Initial commit - add Next.js student portal with Tailwind and Footer component

---
+++ components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav style={{ padding: '1rem', background: '#eee' }}>
+      <h2>New Generation High School - Student Portal</h2>
+    </nav>
+  );
+}
